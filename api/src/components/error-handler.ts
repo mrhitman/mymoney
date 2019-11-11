116d834e530efd74087dfecf55872e68c10233f7
hitman <kabalx47@gmail.com>
2019-11-11 20:45:00 +0200

    V

--- api/src/components/error-handler.ts
+++ api/src/components/error-handler.ts
@@ -4,7 +4,7 @@ async function error_handler(ctx: Koa.Context, next: Koa.Next) {
   try {
     await next();
   } catch (err) {
-    err.status = err.statusCode || err.status || 500;
+    ctx.status = err.statusCode || err.status || 500;
     ctx.body = err.body || err.message;
     ctx.app.emit('error', err, ctx);
   }

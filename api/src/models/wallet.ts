d6a58360eb1f054a8b0b390c94c9e74272ec00d9
hitman <kabalx47@gmail.com>
2019-11-12 21:40:57 +0200

    pockets

--- api/src/models/wallet.ts
+++ api/src/models/wallet.ts
@@ -1,5 +1,11 @@
 import { Model } from 'objection';
 
+export interface Pocket {
+  id: string;
+  amount: number;
+  currency_id: string;
+}
+
 export class Wallet extends Model {
   public id: string;
   public user_id: number;
@@ -7,12 +13,33 @@ export class Wallet extends Model {
   public description: string;
   public cardNumber: string;
   public type: string;
-  public pockets: any;
+  public pockets: Pocket[];
   public last_sync: Date;
   public created_at: Date;
 
   static get tableName() {
     return 'wallets';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        pockets: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              amount: { type: 'number' },
+              currency_id: { type: 'string' },
+            },
+            required: ['id', 'amount', 'currency_id'],
+            additionalProperties: false,
+          },
+        },
+      },
+    };
   }
 }
 

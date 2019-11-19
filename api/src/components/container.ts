8d00ec140ab54617cd0cffb497eae332047fe871
hitman <kabalx47@gmail.com>
2019-11-19 08:58:27 +0200

    container

--- api/src/components/container.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from 'inversify';
-import CategoryProvider from '../modules/category/category-service';
-import CategoryController from '../modules/category/category-controller';
-
-const container = new Container();
-container.bind('category-controller').to(CategoryController);
-container.bind('category').to(CategoryProvider);
-
-export default container;

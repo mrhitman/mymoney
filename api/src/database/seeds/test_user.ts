968fe7b7a7af4b260fef52845327fc4a7e07ee57
Varenik Victor <kabalx47@gmail.com>
2020-05-21 22:18:19 +0300

    more seed

--- api/src/database/seeds/test_user.ts
+++ api/src/database/seeds/test_user.ts
@@ -1,43 +1,57 @@
 /* eslint-disable @typescript-eslint/camelcase */
 
 import bcrypt from 'bcrypt';
-import * as Knex from 'knex';
 import { defaultCategories } from 'common/utils/categories';
+import * as Knex from 'knex';
+import chance from 'chance';
+
+console.log(chance);
 
 export async function seed(knex: Knex): Promise<any> {
-  return knex('users')
-    .del()
-    .then(() => knex('categories').del())
-    .then(() => bcrypt.hash('1', 10))
-    .then((password) => {
-      return knex('users').insert({
-        first_name: 'TEST',
-        last_name: 'USER',
-        email: 'admin@admin.com',
-        password,
-      });
-    })
-    .then(() =>
+  await knex('users').del();
+  await knex('categories').del();
+  const password = await bcrypt.hash('1', 10);
+  const [first_name, middle_name, last_name] = chance().name({ middle_initial: true }).split(' ');
+  await knex('users').insert({
+    first_name,
+    middle_name,
+    last_name,
+    email: 'admin@admin.com',
+    password,
+  });
+  const { id } = await knex
+    .select(['id'])
+    .from('users')
+    .where({ email: 'admin@admin.com' })
+    .first<{ id: number }>();
+  await Promise.all(
+    defaultCategories.map((category) =>
       knex
-        .select(['id'])
-        .from('users')
-        .where({ email: 'admin@admin.com' })
-        .first(),
+        .insert({
+          id: category.id,
+          name: category.name,
+          parent: category.parentId,
+          icon: category.icon,
+          type: category.type,
+          user_id: id,
+        })
+        .into('categories'),
     )
-    .then(({ id }: { id: number }) => {
-      return Promise.all(
-        defaultCategories.map((category) =>
-          knex
-            .insert({
-              id: category.id,
-              name: category.name,
-              parent: category.parentId,
-              icon: category.icon,
-              type: category.type,
-              user_id: id,
-            })
-            .into('categories'),
-        ),
-      );
-    });
+  );
+  await knex.insert({
+    id: chance().guid(),
+    user_id: id,
+    name: 'TEST_WALLET 1',
+    description: 'TEST_DESCRIPTION',
+    type: 'fiat',
+    pockets: '[]'
+  }).into('wallets');
+  await knex.insert({
+    id: chance().guid(),
+    user_id: id,
+    name: 'TEST_WALLET 2',
+    description: 'TEST_DESCRIPTION',
+    type: 'credit',
+    pockets: '[]'
+  }).into('wallets');
 }

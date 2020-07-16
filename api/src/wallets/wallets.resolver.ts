7b5a1d55a4ff911fcbacc9bc21ac10b88c7768d8
One <kabalx47@gmail.com>
2020-07-16 12:27:11 +0300

    pretty code

--- api/src/wallets/wallets.resolver.ts
+++ api/src/wallets/wallets.resolver.ts
@@ -3,29 +3,29 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CurrentUser } from 'src/auth/current-user';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.quard';
 import User from 'src/database/models/user.model';
+import { Wallet } from './dto/wallet';
 import { WalletCreate } from './input/wallet-create';
 import { WalletUpdate } from './input/wallet-update';
-import { Wallet as WalletQuery } from './dto/wallet';
 import { WalletsService } from './wallets.service';
 
-@Resolver((of) => WalletQuery)
+@Resolver((of) => Wallet)
 export class WalletsResolver {
   constructor(private readonly service: WalletsService) {}
 
   @UseGuards(GqlAuthGuard)
-  @Query((returns) => [WalletQuery])
+  @Query((returns) => [Wallet])
   async wallets(@CurrentUser() user: User) {
     return this.service.getAll(user);
   }
 
   @UseGuards(GqlAuthGuard)
-  @Query((returns) => WalletQuery)
+  @Query((returns) => Wallet)
   async wallet(@CurrentUser() user: User, @Args('id') id: string) {
     return this.service.findOne(user, id);
   }
 
   @UseGuards(GqlAuthGuard)
-  @Mutation((returns) => WalletQuery)
+  @Mutation((returns) => Wallet)
   async createWallet(
     @CurrentUser() user: User,
     @Args('walletCreateData')
@@ -35,7 +35,7 @@ export class WalletsResolver {
   }
 
   @UseGuards(GqlAuthGuard)
-  @Mutation((returns) => WalletQuery)
+  @Mutation((returns) => Wallet)
   async updateWallet(
     @CurrentUser() user: User,
     @Args('walletUpdateData')
@@ -45,7 +45,7 @@ export class WalletsResolver {
   }
 
   @UseGuards(GqlAuthGuard)
-  @Mutation((returns) => WalletQuery)
+  @Mutation((returns) => Wallet)
   async deleteWallet(@CurrentUser() user: User, @Args('id') id: string) {
     return this.service.delete(user, id);
   }

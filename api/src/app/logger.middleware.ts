2257430ecc2cd2a2c00c4ab35d413998060d28a9
One <kabalx47@gmail.com>
2020-07-17 08:54:04 +0300

    pretty logger

--- api/src/app/logger.middleware.ts
+++ api/src/app/logger.middleware.ts
@@ -1,10 +1,10 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
-    global.console.log(`[${new Date()}] ${req.method} ${req.path}`);
+    Logger.log(`${req.method} ${req.path}`, 'Route');
     next();
   }
 }

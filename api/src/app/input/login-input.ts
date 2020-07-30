09ecb2332d4fc30e45271d20b795ec4dd50bde87
mrhitman <kabalx47@gmail.com>
2020-07-30 16:53:47 +0300

    add few validations

--- api/src/app/input/login-input.ts
+++ api/src/app/input/login-input.ts
@@ -1,10 +1,13 @@
-import { Field, Float, InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
 
 @InputType()
 export class LoginInput {
   @Field(() => String)
+  @IsString()
   email: string;
 
   @Field((type) => String)
+  @IsString()
   password: string;
 }
--- api/src/app/input/register-input.ts
+++ api/src/app/input/register-input.ts
@@ -1,19 +1,27 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsString, IsOptional, IsEmail } from 'class-validator';
 
 @InputType()
 export class RegisterInput {
   @Field()
+  @IsString()
   firstName: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   middleName?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   lastName?: string;
 
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @IsString()
   password: string;
 }

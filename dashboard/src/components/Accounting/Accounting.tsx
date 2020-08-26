00e46889a64db62422efc9523f2c0ce1a7e81c9f
One <kabalx47@gmail.com>
2020-08-26 18:54:17 +0300

    accounting page

--- dashboard/src/components/Accounting/Accounting.tsx
+++ dashboard/src/components/Accounting/Accounting.tsx
@@ -1,9 +1,29 @@
 import { useQuery } from '@apollo/client';
-import { Skeleton, Card, List, Typography, Divider } from 'antd';
+import {
+  Skeleton,
+  Card,
+  List,
+  Typography,
+  Divider,
+  Avatar,
+  Row,
+  Col,
+} from 'antd';
 import { loader } from 'graphql.macro';
 import React, { FC } from 'react';
 
 const WalletsQuery = loader('src/queries/wallets.graphql');
+
+const icons = {
+  'monobank-black':
+    'https://www.monobank.com.ua/resources/static-1/img/logo-medium-192x192.png',
+  'Privat24 Card':
+    'https://xpc.com.ua/image/catalog/general/page/a_icon/privat24.svg',
+  'default-card':
+    'https://www.nicepng.com/png/full/104-1044427_png-library-download-drawing-at-getdrawings-com-free.png',
+};
+
+const layout = { xs: 24, sm: 24, md: 12, lg: 6 };
 
 export const Accounting: FC = () => {
   const { loading, error, data } = useQuery(WalletsQuery);
@@ -11,27 +31,35 @@ export const Accounting: FC = () => {
 
   return (
     <Skeleton loading={loading}>
-      {wallets.map((wallet: any) => {
-        return (
-          <Card
-            id={wallet.id}
-            style={{ width: 300, marginTop: 16 }}
-            loading={loading}
-          >
-            <Card.Meta title={wallet.name} description={wallet.description} />
-            <Divider />
-            <List
-              dataSource={wallet.pockets}
-              renderItem={(pocket: any) => (
-                <Typography>
-                  {pocket.currency.symbol} {pocket.amount}{' '}
-                  {pocket.currency.name}
-                </Typography>
-              )}
-            />
-          </Card>
-        );
-      })}
+      <Row gutter={16}>
+        {wallets.map((wallet: any) => {
+          return (
+            <Col id={wallet.id} {...layout}>
+              <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+                <Card.Meta
+                  title={wallet.name}
+                  description={wallet.description}
+                  avatar={
+                    <Avatar
+                      src={(icons as any)[wallet.type || 'default-card']}
+                    />
+                  }
+                />
+                <Divider />
+                <List
+                  dataSource={wallet.pockets}
+                  renderItem={(pocket: any) => (
+                    <Typography>
+                      {pocket.currency.symbol} {pocket.amount}{' '}
+                      {pocket.currency.name}
+                    </Typography>
+                  )}
+                />
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </Skeleton>
   );
 };

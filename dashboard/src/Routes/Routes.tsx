41b43c21fff414ce18efad6e7ad403056638cc96
One <kabalx47@gmail.com>
2020-08-29 17:05:48 +0300

    use hash router

--- dashboard/src/Routes/Routes.tsx
+++ dashboard/src/Routes/Routes.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Categories from 'src/components/Categories/Categories';
 import AnalysisByCategory from 'src/components/Analysis/AnalysisByCategory';
 import AnalysisByPeriod from 'src/components/Analysis/AnalysisByPeriod';

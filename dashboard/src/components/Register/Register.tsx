47e6ecac2354408d87d945464805f86820ecfd89
mrhitman <kabalx47@gmail.com>
2020-08-30 18:47:17 +0300

    register form

--- dashboard/src/components/Register/Register.tsx
+++ dashboard/src/components/Register/Register.tsx
@@ -1,55 +1,79 @@
+import { EyeInvisibleOutlined, EyeTwoTone, RollbackOutlined } from '@ant-design/icons';
+import { useMutation } from '@apollo/client';
+import { Button, Col, Input, Layout, Row, Space } from 'antd';
+import { loader } from 'graphql.macro';
 import React from 'react';
 import {
   GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
+  GoogleLoginResponse
 } from 'react-google-login';
-import { Row, Col, Layout, Space, Button } from 'antd';
-import { Link } from 'react-router-dom';
-import {
-  BackwardFilled,
-  StepBackwardFilled,
-  RollbackOutlined,
-} from '@ant-design/icons';
+import { Link, Redirect } from 'react-router-dom';
+import { RegisterMutation } from 'src/generated/graphql';
 
-function onSignIn(response: GoogleLoginResponse | GoogleLoginResponseOffline) {
-  if ('getBasicProfile' in response) {
-    const profile = response.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-    const id_token = response.getAuthResponse().id_token;
-    console.log('ID Token: ' + id_token);
-  }
-}
 
+const RegisterQuery = loader('src/queries/register.graphql');
 export const Register: React.FC = () => {
+  const [register] = useMutation<RegisterMutation>(RegisterQuery);
+  const [profile, setProfile] = React.useState<GoogleLoginResponse>();
+  const [password, setPassword] = React.useState('');
+  const [done, setDone] = React.useState(false);
+
+  function finishRegister(response: GoogleLoginResponse) {
+    const profile = response.getBasicProfile();
+    register({
+      variables: {
+        email: profile.getEmail(),
+        firstName: profile.getFamilyName(),
+        lastName: profile.getGivenName(),
+        password
+      }
+    });
+  }
+
+  if (done) {
+    return <Redirect to={'/login'} exact />
+  }
+
   return (
     <Layout>
       <Layout.Content>
         <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
-          <Row align="middle">
-            <Col span={24}>
-              <Link to="/login">
-                <Button
-                  icon={<RollbackOutlined />}
-                  size="large"
-                  style={{ width: 180.46, height: 46 }}
-                >
-                  back
-                </Button>
-              </Link>
-            </Col>
-            <Col>
-              <GoogleLogin
-                clientId="172140808548-71g0juh12o6o8jltjkp9pnmq76lkht9v"
-                onSuccess={onSignIn}
+          {profile ? (
+            <Space direction="vertical">
+              <Input.Password
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                placeholder="input password"
+                suffix={(visible: boolean) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
               />
-            </Col>
-          </Row>
+              <Button onClick={() => finishRegister(profile)}>Finish registration</Button>
+            </Space>
+          ) : (
+              <Row align="middle">
+                <Col span={24}>
+                  <Link to="/login">
+                    <Button
+                      icon={<RollbackOutlined />}
+                      size="large"
+                      style={{ width: 180.46, height: 46 }}
+                    >
+                      back
+                </Button>
+                  </Link>
+                </Col>
+                <Col>
+                  <GoogleLogin
+                    clientId="172140808548-71g0juh12o6o8jltjkp9pnmq76lkht9v"
+                    onSuccess={(response) => {
+                      if ('getBasicProfile' in response) {
+                        setProfile(response);
+                      }
+                    }}
+                  />
+                </Col>
+              </Row>
+            )}
+
         </Row>
       </Layout.Content>
     </Layout>
--- dashboard/src/generated/graphql.tsx
+++ dashboard/src/generated/graphql.tsx
@@ -584,6 +584,23 @@ export type RefreshMutation = (
   ) }
 );
 
+export type RegisterMutationVariables = Exact<{
+  email: Scalars['String'];
+  password: Scalars['String'];
+  firstName: Scalars['String'];
+  middleName?: Maybe<Scalars['String']>;
+  lastName?: Maybe<Scalars['String']>;
+}>;
+
+
+export type RegisterMutation = (
+  { __typename?: 'Mutation' }
+  & { register: (
+    { __typename?: 'User' }
+    & Pick<User, 'id'>
+  ) }
+);
+
 export type WalletFragment = (
   { __typename?: 'Wallet' }
   & Pick<Wallet, 'id' | 'name' | 'type' | 'description'>
@@ -867,6 +884,42 @@ export function useRefreshMutation(baseOptions?: Apollo.MutationHookOptions<Refr
 export type RefreshMutationHookResult = ReturnType<typeof useRefreshMutation>;
 export type RefreshMutationResult = Apollo.MutationResult<RefreshMutation>;
 export type RefreshMutationOptions = Apollo.BaseMutationOptions<RefreshMutation, RefreshMutationVariables>;
+export const RegisterDocument = gql`
+    mutation Register($email: String!, $password: String!, $firstName: String!, $middleName: String, $lastName: String) {
+  register(registerData: {email: $email, password: $password, firstName: $firstName, middleName: $middleName, lastName: $lastName}) {
+    id
+  }
+}
+    `;
+export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutation, { data, loading, error }] = useRegisterMutation({
+ *   variables: {
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *      firstName: // value for 'firstName'
+ *      middleName: // value for 'middleName'
+ *      lastName: // value for 'lastName'
+ *   },
+ * });
+ */
+export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
+        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, baseOptions);
+      }
+export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
+export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
 export const GetTransactionsDocument = gql`
     query getTransactions($type: TransactionType, $limit: Float, $offset: Float, $walletId: String) {
   transactions(type: $type, limit: $limit, offset: $offset, walletId: $walletId) {

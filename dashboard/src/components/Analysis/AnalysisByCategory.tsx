4bf8523dce4326d9d8065793f3e5b01ef9f35295
mrhitman <kabalx47@gmail.com>
2020-09-20 12:27:04 +0300

    filtering by period

--- dashboard/src/components/Analysis/AnalysisByCategory.tsx
+++ dashboard/src/components/Analysis/AnalysisByCategory.tsx
@@ -1,5 +1,6 @@
 import { SyncOutlined } from '@ant-design/icons';
-import { Dropdown, Menu, Button, Row, Col, Spin } from 'antd';
+import { Dropdown, Menu, Button, Row, Col, Spin, DatePicker } from 'antd';
+import moment from 'moment';
 import React, { FC, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { TransactionType, useAnalysByCategoriesQuery } from 'src/generated/graphql';
@@ -7,17 +8,23 @@ import { VictoryPie } from 'victory';
 
 export const AnalysisByCategory: FC = () => {
   const [type, setType] = useState<TransactionType>(TransactionType.Income);
+  const [from, setFrom] = useState<number | undefined>(moment().startOf('month').unix());
+  const [to, setTo] = useState<number | undefined>(moment().unix());
   const { t } = useTranslation();
-  const { loading, data, error, refetch } = useAnalysByCategoriesQuery({ variables: { type } });
+  const { loading, data, error, refetch } = useAnalysByCategoriesQuery({ variables: { type, from, to } });
 
   useEffect(() => {
-    refetch({ type });
+    refetch({ type, from, to });
   }, [type]);
 
   return (
     <>
-      <Row>
-        <Col>
+      <Row >
+        <Col span={6} >
+          <DatePicker.RangePicker showTime onCalendarChange={(values) => {
+            setFrom(values?.[0]?.unix());
+            setTo(values?.[1]?.unix());
+          }} />
           <Dropdown
             overlay={() => (
               <Menu>
@@ -32,30 +39,34 @@ export const AnalysisByCategory: FC = () => {
             trigger={['click']}
             placement="bottomCenter"
           >
-            <Button className="ant-dropdown-link" onClick={() => refetch({ type })}>{type}</Button>
+            <Button className="ant-dropdown-link" onClick={() => refetch({ type, from, to })}>{type}</Button>
           </Dropdown>
           <Button >
             <SyncOutlined />
           </Button>
         </Col>
-      </Row>
-      <Row align="middle" justify="center" gutter={[10, 10]} >
-        <Col span={12}>
-          <Spin spinning={loading}>
-            <VictoryPie
-              colorScale={'qualitative'}
-              style={{ labels: { fill: 'black' } }}
-              innerRadius={80}
-              labelRadius={120}
-              cornerRadius={2}
-              labelPosition={'centroid'}
-              labels={({ datum }) => `${t(datum.x)}`}
-              data={data?.statisticByCategory.map(data => ({
-                x: data.category.name,
-                y: Math.abs(data.amount)
-              }))}
-            />
-          </Spin>
+        <Col span={18} >
+          <Row align="middle" justify="center" gutter={[10, 10]} >
+            <Col span={12}>
+              <Spin spinning={loading}>
+                <VictoryPie
+                  colorScale={'qualitative'}
+                  style={{ labels: { fill: 'black' } }}
+                  innerRadius={80}
+                  labelRadius={120}
+                  cornerRadius={2}
+                  labelPosition={'centroid'}
+                  labels={({ datum }) => `${t(datum.x)}`}
+                  data={data?.statisticByCategory
+                    // .filter(data => data.category.name !== 'SYSTEM_EMPTY')
+                    .map(data => ({
+                      x: data.category.name,
+                      y: Math.abs(data.amount)
+                    }))}
+                />
+              </Spin>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </>

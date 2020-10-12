99b87d95692d720792347d602384c9c04b64675c
mrhitman <kabalx47@gmail.com>
2020-10-12 17:10:14 +0300

    correct refresh token

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,37 +1,53 @@
 import { ApolloProvider } from '@apollo/client';
 import 'antd/dist/antd.css';
 import ApolloClient from 'apollo-boost';
+import { Observable } from 'apollo-link';
 import React, { FC } from 'react';
+import Routes from 'src/components/misc/Routes';
 import './App.css';
+import { RefreshDocument, RefreshMutation } from './generated/graphql';
 import './i18n';
 import IconStyles from './IconStyles';
-import Routes from 'src/components/misc/Routes';
-import { RefreshDocument, RefreshMutation } from './generated/graphql';
-import history from './history';
+import history from './history'
+
 
 const uri = process.env.REACT_APP_SERVER + 'graphql';
 const client = new ApolloClient({
-  uri, onError: (error) => {
+  uri,
+  onError: (error) => {
+
     if (error.response?.errors?.some(e => e.extensions?.exception?.status === 401)) {
-      client.mutate<RefreshMutation>({
-        mutation: RefreshDocument, variables: {
-          token: localStorage.getItem('refreshToken')
-        }
-      }).then(response => {
-        const { accessToken, refreshToken } = response.data?.refresh!;
-        localStorage.setItem('accessToken', accessToken!);
-        localStorage.setItem('refreshToken', refreshToken!);
-        return error.operation.setContext(({ headers = {} }: any) => ({
-          headers: {
-            ...headers as any,
-            authorization: `Bearer ${accessToken}` || null,
+      return new Observable(observer => {
+        client.mutate<RefreshMutation>({
+          mutation: RefreshDocument, variables: {
+            token: localStorage.getItem('refreshToken')
           }
-        }));
-      })
-        .catch(response => {
-          localStorage.clear();
-          history.push('/login');
+        }).then(response => {
+          const { accessToken, refreshToken } = response.data?.refresh!;
+          localStorage.setItem('accessToken', accessToken!);
+          localStorage.setItem('refreshToken', refreshToken!);
+
+          const oldHeaders = error.operation.getContext().headers;
+          error.operation.setContext({
+            headers: {
+              ...oldHeaders,
+              Authorization: accessToken
+            },
+          });
+
+          const subscriber = {
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          }
+
+          return error.forward(error.operation).subscribe(subscriber);
         })
+          .catch(() => {
+            localStorage.clear();
+            history.push('/login');
+          })
+      });
     }
   }
 });

10ec445be6c1f5c6ab8689e6e0798805f5c211ce
One <kabalx47@gmail.com>
2020-10-24 23:16:58 +0300

    fix addconnector close modal

--- dashboard/src/components/Connectors/AddConnector.tsx
+++ dashboard/src/components/Connectors/AddConnector.tsx
@@ -1,7 +1,14 @@
-import { Modal, Input, Form, Switch, Select, Divider } from 'antd'
-import React from 'react'
-import { Formik } from 'formik'
-import { useAddConnectorMutation } from 'src/generated/graphql'
+import {
+    Divider,
+    Form,
+    Input,
+    Modal,
+    Select,
+    Switch
+} from 'antd';
+import { useFormik } from 'formik';
+import React from 'react';
+import { useAddConnectorMutation } from 'src/generated/graphql';
 
 interface AddConnectorProps {
     show: boolean;
@@ -16,77 +23,79 @@ export const AddConnector: React.FC<AddConnectorProps> = ({ show, onClose, onSub
                 Authorization: localStorage.getItem('accessToken')
             }
         }
-    })
-    return (
-        <Formik
-            initialValues={{
-                type: "monobank",
-                description: "",
-                token: "",
-                password: "",
-                merchantId: "",
-                enabled: true,
-                interval: 900
-            }}
-            onSubmit={values => {
-                addConector({
-                    variables: {
-                        type: values.type,
-                        description: values.description,
-                        interval: 900,
-                        enabled: values.enabled,
-                        params: {
-                            token: values.token,
-                            merchantId: values.merchantId,
-                            password: values.password
-                        }
+    });
+    const formik = useFormik({
+        initialValues: {
+            type: "monobank",
+            description: "",
+            token: "",
+            password: "",
+            merchantId: "",
+            enabled: true,
+            interval: 900
+        },
+        onSubmit: values => {
+            addConector({
+                variables: {
+                    type: values.type,
+                    description: values.description,
+                    interval: 900,
+                    enabled: values.enabled,
+                    params: {
+                        token: values.token,
+                        merchantId: values.merchantId,
+                        password: values.password
                     }
-                });
+                }
+            });
+            onClose();
+            onSubmit();
+        }
+    });
+
+    return (
+        <Modal
+            title={`Add ${formik.values.type} Connector`}
+            visible={show}
+            onOk={() => formik.handleSubmit()}
+            onCancel={e => {
+                e.stopPropagation();
                 onClose();
-                onSubmit();
             }}
-            render={(bag) => (
-                <Modal
-                    title={`Add ${bag.values.type} Connector`}
-                    visible={show}
-                    onOk={() => bag.handleSubmit()}
-                    onCancel={onClose}
-                    width={'80%'}
-                    style={{ maxWidth: 900 }}
-                    closable
-                >
-                    <Form labelCol={{ span: 8 }}>
-                        <Form.Item label='Description' name='description'>
-                            <Input value={bag.values.description} onChange={bag.handleChange('description')} />
-                        </Form.Item>
-                        <Form.Item label='Type' name='type'>
-                            <Select value={bag.values.type} defaultValue={bag.initialValues.type} onChange={bag.handleChange('type')}>
-                                <Select.Option value="monobank">Monobank</Select.Option>
-                                <Select.Option value="privat24">Privat24</Select.Option>
-                            </Select>
-                        </Form.Item>
-                        {bag.values.type === 'monobank' && <Form.Item label='Token' name='token'>
-                            <Input value={bag.values.token} onChange={bag.handleChange('token')} />
-                        </Form.Item>}
-                        {bag.values.type === 'privat24' && <>
-                            <Form.Item label='Password' name='password'>
-                                <Input value={bag.values.password} onChange={bag.handleChange('password')} />
-                            </Form.Item>
-                            <Form.Item label='Merchant Id' name='merchantId'>
-                                <Input value={bag.values.merchantId} onChange={bag.handleChange('merchantId')} />
-                            </Form.Item>
-                        </>}
-                        <Form.Item label="Enable sync"  >
-                            <div style={{ float: 'right' }}>
-                                <Switch checked={bag.values.enabled} onChange={checked => bag.setFieldValue('enabled', checked)} />
-                            </div>
-                        </Form.Item>
-                        <Divider />
-                    </Form>
-                </Modal>
-            )}
-        />
-    )
+            width={'80%'}
+            style={{ maxWidth: 900 }}
+            closable
+        >
+            <Form labelCol={{ span: 8 }}>
+                <Form.Item label='Description' name='description'>
+                    <Input value={formik.values.description} onChange={e => formik.setFieldValue('description', e.target.value)} />
+                </Form.Item>
+                <Form.Item label='Type' name='type'>
+                    <Select value={formik.values.type} defaultValue={formik.initialValues.type} onChange={value => formik.setFieldValue('type', value)}>
+                        <Select.Option value="monobank">Monobank</Select.Option>
+                        <Select.Option value="privat24">Privat24</Select.Option>
+                    </Select>
+                </Form.Item>
+                {formik.values.type === 'monobank' && <Form.Item label='Token' name='token'>
+                    <Input value={formik.values.token} onChange={e => formik.setFieldValue('token', e.target.value)} />
+                </Form.Item>}
+                {formik.values.type === 'privat24' && <>
+                    <Form.Item label='Password' name='password'>
+                        <Input value={formik.values.password} onChange={e => formik.setFieldValue('password', e.target.value)} />
+                    </Form.Item>
+                    <Form.Item label='Merchant Id' name='merchantId'>
+                        <Input value={formik.values.merchantId} onChange={e => formik.setFieldValue('merchantId', e.target.value)} />
+                    </Form.Item>
+                </>}
+                <Form.Item label="Enable sync">
+                    <div style={{ float: 'right' }}>
+                        <Switch checked={formik.values.enabled} onChange={checked => formik.setFieldValue('enabled', checked)} />
+                    </div>
+                </Form.Item>
+                <Divider />
+            </Form>
+        </Modal>
+    );
 }
 
 export default AddConnector;

f93816bb1637beb6168ca1564b8eb080c96e6efd
One <kabalx47@gmail.com>
2020-11-29 22:30:04 +0200

    show categories dinamic

---
+++ dashboard/src/components/MainPage/CategoriesDinamic.tsx
@@ -0,0 +1,69 @@
+import { round } from 'lodash';
+import moment from 'moment';
+import React, { FC, useEffect, useState } from 'react';
+import {
+  useAnalysByCategoriesLazyQuery,
+  AnalysByCategoriesQuery,
+  TransactionType,
+} from '../../generated/graphql';
+import { Chart, Interval, Tooltip, Axis, Coordinate, Interaction } from 'bizcharts';
+import { useTranslation } from 'react-i18next';
+
+const cols = {
+  percent: {
+    formatter: (val: number) => round(val * 100, 2) + '%',
+  },
+};
+
+function getSum(data?: AnalysByCategoriesQuery) {
+  return data ? data.statisticByCategory.reduce((acc, s) => acc + Math.abs(s.amount), 0) : 1;
+}
+
+export const CategoriesDinamic: FC = () => {
+  const { t } = useTranslation();
+  const [getData, { data }] = useAnalysByCategoriesLazyQuery({
+    variables: {
+      from: moment().subtract(1, 'month').unix(),
+      type: TransactionType.Outcome,
+    },
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Chart
+      height={400}
+      data={data?.statisticByCategory.map((s) => ({
+        item: t(s.category.name),
+        count: round(Math.abs(s.amount), 2),
+        percent: Math.abs(s.amount) / getSum(data),
+      }))}
+      scale={cols}
+      autoFit
+    >
+      <Coordinate type="theta" radius={0.75} />
+      <Tooltip showTitle={false} />
+      <Axis visible={false} />
+      <Interval
+        position="percent"
+        adjust="stack"
+        color="item"
+        style={{
+          lineWidth: 1,
+          stroke: '#fff',
+        }}
+        label={[
+          'count',
+          {
+            content: (data) => `${data.item}: ${round(data.percent * 100, 2)}%`,
+          },
+        ]}
+      />
+      <Interaction type="element-single-selected" />
+    </Chart>
+  );
+};
+
+export default CategoriesDinamic;
--- dashboard/src/components/MainPage/MainPage.tsx
+++ dashboard/src/components/MainPage/MainPage.tsx
@@ -2,16 +2,22 @@ import { Col, Row } from 'antd';
 import React, { FC } from 'react';
 import LastTransactions from './LastTransactions';
 import WalletsDinamic from './WalletsDinamic';
+import CategoriesDinamic from './CategoriesDinamic';
 
 export const MainPage: FC = () => {
   return (
     <>
-      <Row>
+      <Row gutter={20}>
         <Col span={12}>
           <LastTransactions count={10} />
         </Col>
         <Col span={12}>
-          <WalletsDinamic />
+          <Row gutter={20}>
+            <WalletsDinamic />
+          </Row>
+          <Row gutter={20}>
+            <CategoriesDinamic />
+          </Row>
         </Col>
       </Row>
     </>

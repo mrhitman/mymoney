0a9fa38f13465b299eae03be6dc9c79422d638ef
One <kabalx47@gmail.com>
2020-12-07 12:11:21 +0200

    hotfix ranges

--- dashboard/src/components/misc/DateRanges.ts
+++ dashboard/src/components/misc/DateRanges.ts
@@ -1,12 +1,11 @@
 import moment from 'moment';
 
-const today = moment().utc();
 const ranges: Record<string, [moment.Moment, moment.Moment]> = {
-  Today: [today.startOf('day'), today.endOf('day')],
-  'This Week': [today.startOf('week'), today.endOf('week')],
-  'This Month': [today.startOf('month'), today.endOf('month')],
-  'Last 7 Days': [today.subtract(7, 'days'), today.endOf('day')],
-  'Last 30 Days': [today.subtract(30, 'days'), today.endOf('day')],
+  Today: [moment().utc().startOf('day'), moment().utc().endOf('day')],
+  'This Week': [moment().utc().startOf('week'), moment().utc().endOf('week')],
+  'This Month': [moment().utc().startOf('month'), moment().utc().endOf('month')],
+  'Last 7 Days': [moment().utc().subtract(7, 'days'), moment().utc().endOf('day')],
+  'Last 30 Days': [moment().utc().subtract(30, 'days'), moment().utc().endOf('day')],
 };
 
 export default ranges;

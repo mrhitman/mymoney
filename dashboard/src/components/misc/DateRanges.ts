a8359de97d7fa37d64e6e27740ab8c3fb277035f
One <kabalx47@gmail.com>
2020-12-10 12:37:31 +0200

    remove lerna

--- dashboard/src/components/misc/DateRanges.ts
+++ dashboard/src/components/misc/DateRanges.ts
@@ -1,11 +1,11 @@
-import moment from 'moment';
+import { Moment, utc } from 'moment';
 
-const ranges: Record<string, [moment.Moment, moment.Moment]> = {
-  Today: [moment().utc().startOf('day'), moment().utc().endOf('day')],
-  'This Week': [moment().utc().startOf('week'), moment().utc().endOf('week')],
-  'This Month': [moment().utc().startOf('month'), moment().utc().endOf('month')],
-  'Last 7 Days': [moment().utc().subtract(7, 'days'), moment().utc().endOf('day')],
-  'Last 30 Days': [moment().utc().subtract(30, 'days'), moment().utc().endOf('day')],
+const ranges: Record<string, [Moment, Moment]> = {
+  Today: [utc().startOf('day'), utc().endOf('day')],
+  'This Week': [utc().startOf('week'), utc().endOf('week')],
+  'This Month': [utc().startOf('month'), utc().endOf('month')],
+  'Last 7 Days': [utc().subtract(7, 'days'), utc().endOf('day')],
+  'Last 30 Days': [utc().subtract(30, 'days'), utc().endOf('day')],
 };
 
 export default ranges;

5b0fc21b7d2107a93fdbbc8e61188faa4052e12a
One <kabalx47@gmail.com>
2021-01-02 17:00:49 +0200

    allow update profile

--- dashboard/src/components/Settings/Settings.tsx
+++ dashboard/src/components/Settings/Settings.tsx
@@ -1,13 +1,10 @@
-import { Col, Row, Select, Button } from 'antd';
+import { Col, Row, Select } from 'antd';
 import React, { FC } from 'react';
-import { useGetProfileQuery } from 'src/generated/graphql';
 import { useTranslation } from 'react-i18next';
-import { EditOutlined } from '@ant-design/icons';
+import { UpdateProfile } from './UpdateProfile';
 
 const Settings: FC = () => {
-  const { data } = useGetProfileQuery();
   const { t, i18n } = useTranslation();
-
   return (
     <Row>
       <Col span={7}>
@@ -18,41 +15,19 @@ const Settings: FC = () => {
           alt="profile img"
         />
       </Col>
-      <Col span={17}>
-        <Row>
-          <Col span={4}>{t('first_name')}:</Col>
-          <Col>{data?.profile.firstName}</Col>
-        </Row>
-        <Row>
-          <Col span={4}>{t('last_name')}:</Col>
-          <Col>{data?.profile.lastName}</Col>
-        </Row>
-        <Row>
-          <Col span={4}>{t('middle_name')}:</Col>
-          <Col>{data?.profile.middleName}</Col>
-        </Row>
-        <Row>
-          <Col span={4}>Email</Col>
-          <Col>{data?.profile.email}</Col>
-        </Row>
-        <Button icon={<EditOutlined />}>
-          Update
-        </Button>
-        <Row>
-          <Col span={4}>{t('language')}</Col>
-          <Col>
-            <Select
-              onChange={(e: string) => {
-                i18n.changeLanguage(e);
-                localStorage.setItem('lng', e);
-              }}
-              value={localStorage.getItem('lng') || 'ru'}
-            >
-              <Select.Option value="en">English</Select.Option>
-              <Select.Option value="ru">Русский</Select.Option>
-            </Select>
-          </Col>
-        </Row>
+      <UpdateProfile />
+      <Col span={4}>{t('language')}</Col>
+      <Col>
+        <Select
+          onChange={(e: string) => {
+            i18n.changeLanguage(e);
+            localStorage.setItem('lng', e);
+          }}
+          value={localStorage.getItem('lng') || 'ru'}
+        >
+          <Select.Option value="en">English</Select.Option>
+          <Select.Option value="ru">Русский</Select.Option>
+        </Select>
       </Col>
     </Row>
   );
--- dashboard/src/components/Settings/UpdateProfile.tsx
+++ dashboard/src/components/Settings/UpdateProfile.tsx
@@ -1,61 +1,102 @@
-import React, { FC } from 'react';
-import { Form, Input } from 'antd';
+import { EditOutlined, SaveOutlined } from '@ant-design/icons';
+import { Button, Col, Form, Input } from 'antd';
 import { useFormik } from 'formik';
-import { useUpdateProfileMutation } from 'src/generated/graphql';
+import React, { FC, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useGetProfileQuery, useUpdateProfileMutation } from 'src/generated/graphql';
+import { omit } from 'lodash';
 
 export const UpdateProfile: FC = () => {
-    const [updateProfile] = useUpdateProfileMutation();
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            middleName: '',
-            oldPassword: '',
-            newPassword: ''
+  const [updateProfile] = useUpdateProfileMutation();
+  const { data } = useGetProfileQuery();
+  const { t } = useTranslation();
+  const [editing, setEditing] = useState(false);
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      firstName: data?.profile.firstName || '',
+      lastName: data?.profile.lastName || '',
+      middleName: data?.profile.middleName || '',
+      oldPassword: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: (values) => {
+      if (values.password && values.password !== values.confirmPassword) {
+        return;
+      }
+
+      updateProfile({
+        variables: {
+          profileUpdateData: omit(values, ['confirmPassword']),
         },
-        onSubmit: values => {
-            updateProfile({
-                variables: {
-                    profileUpdateData: values
-                }
-            })
-        }
-    });
+      });
+    },
+  });
 
-
-    return (
-        <div>
-            <Form>
-                <Form.Item label="First name">
-                    <Input
-                        value={formik.values.firstName}
-                        onChange={e => formik.setFieldValue('firstName', e.target.value)}
-                    />
-                </Form.Item>
-                <Form.Item label="Last name">
-                    <Input
-                        value={formik.values.lastName}
-                        onChange={e => formik.setFieldValue('lastName', e.target.value)}
-                    />
-                </Form.Item>
-                <Form.Item label="Middle name">
-                    <Input
-                        value={formik.values.middleName}
-                        onChange={e => formik.setFieldValue('middleName', e.target.value)}
-                    />
-                </Form.Item>
-                <Form.Item label="New Password">
-                    <Input
-                        type="password"
-                        onChange={e => formik.setFieldValue('newPassword', e.target.value)}
-                    />
-                </Form.Item>
-                <Form.Item label="Old Password">
-                    <Input
-                        type="password"
-                        onChange={e => formik.setFieldValue('oldPassword', e.target.value)}
-                    />
-                </Form.Item>
-            </Form>
-        </div>);
-}
+  return (
+    <Col span={17}>
+      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 12 }}>
+        <Form.Item label={t('first_name')}>
+          <Input
+            value={formik.values.firstName}
+            onChange={(e) => formik.setFieldValue('firstName', e.target.value)}
+            readOnly={!editing}
+          />
+        </Form.Item>
+        <Form.Item label={t('last_name')}>
+          <Input
+            value={formik.values.lastName}
+            onChange={(e) => formik.setFieldValue('lastName', e.target.value)}
+            readOnly={!editing}
+          />
+        </Form.Item>
+        <Form.Item label={t('middle_name')}>
+          <Input
+            value={formik.values.middleName}
+            onChange={(e) => formik.setFieldValue('middleName', e.target.value)}
+            readOnly={!editing}
+          />
+        </Form.Item>
+        <Form.Item label={t('email')}>
+          <Input value={data?.profile.email} readOnly />
+        </Form.Item>
+        <Form.Item label={t('old_password')}>
+          <Input
+            type="password"
+            onChange={(e) => formik.setFieldValue('oldPassword', e.target.value)}
+            readOnly={!editing}
+          />
+        </Form.Item>
+        <Form.Item label={t('password')}>
+          <Input
+            type="password"
+            onChange={(e) => formik.setFieldValue('password', e.target.value)}
+            readOnly={!editing}
+          />
+        </Form.Item>
+        <Form.Item label={t('confirm_password')}>
+          <Input
+            type="password"
+            onChange={(e) => formik.setFieldValue('confirmPassword', e.target.value)}
+            readOnly={!editing}
+          />
+        </Form.Item>
+      </Form>
+      <Col offset={10}>
+        <Button icon={<EditOutlined />} onClick={() => setEditing(true)}>
+          Edit
+        </Button>
+        <Button
+          icon={<SaveOutlined />}
+          onClick={() => {
+            setEditing(false);
+            formik.handleSubmit();
+          }}
+        >
+          Save
+        </Button>
+      </Col>
+    </Col>
+  );
+};

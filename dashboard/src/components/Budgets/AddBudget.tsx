1b942c69f2a7a5f322f3a3c25a4a13bb97e6410f
One <kabalx47@gmail.com>
2021-01-26 16:25:56 +0200

    add budget types

--- dashboard/src/components/Budgets/AddBudget.tsx
+++ dashboard/src/components/Budgets/AddBudget.tsx
@@ -3,6 +3,28 @@ import { Button, Modal } from 'antd';
 import { useFormik } from 'formik';
 import React, { FC, useState } from 'react';
 import BudgetForm from './BudgetForm';
+import moment from 'moment';
+
+interface BudgetCategory {
+  categoryId: string;
+  amount: number;
+  progress: number;
+}
+interface BudgetFormValues {
+  date: Date;
+  deadline: Date;
+  currencyId: string;
+  incomes: BudgetCategory[];
+  outcomes: BudgetCategory[];
+}
+
+const initialValues: BudgetFormValues = {
+  date: moment().startOf('month').toDate(),
+  deadline: moment().startOf('month').toDate(),
+  currencyId: '',
+  incomes: [],
+  outcomes: [],
+};
 
 export const AddBudget: FC<{}> = (props) => {
   const [visible, setVisible] = useState(false);

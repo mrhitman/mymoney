876c3f304f811453ac4e6ea6d996dad8fb0c73dc
One <kabalx47@gmail.com>
2021-02-02 15:13:19 +0200

    disable autorestart heroku

--- api/src/app/task.service.ts
+++ api/src/app/task.service.ts
@@ -50,9 +50,9 @@ export class TaskService {
   public async refreshHeroku() {
     Logger.log('Refresh heroku', 'App');
 
-    await Promise.all([
-      axios.get('https://mymoney-server-api.herokuapp.com/graphql').catch(console.log),
-      axios.get('https://mymoney-accounting.herokuapp.com/').catch(console.log),
-    ]);
+    // await Promise.all([
+    // axios.get('https://mymoney-server-api.herokuapp.com/graphql').catch(console.log),
+    // axios.get('https://mymoney-accounting.herokuapp.com/').catch(console.log),
+    // ]);
   }
 }

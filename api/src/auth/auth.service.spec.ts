0f9b5db31ab6c606e1741dab2abc137f0677b6ae
One <kabalx47@gmail.com>
2021-03-05 11:54:17 +0200

    check mock for db

--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -5,6 +5,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { Chance } from 'chance';
 import { DatabaseModule } from 'src/database/database.module';
 import { config } from 'src/config';
+import { User } from 'src/database/models/user.model';
+import { UserCategory } from 'src/database/models/user-category.model';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -18,16 +20,7 @@ describe('AuthService', () => {
           signOptions: { expiresIn: config.app.jwt.expiresIn },
         }),
       ],
-      providers: [
-        {
-          provide: 'KnexConnection',
-          useFactory: async () => {
-            return jest.fn().mockImplementation();
-          },
-        },
-        AuthService,
-        UsersService,
-      ],
+      providers: [AuthService, UsersService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -39,20 +32,32 @@ describe('AuthService', () => {
 
   describe(' register User', () => {
     it(' successfully', async () => {
-      const chance = new Chance();
-      const firstName = chance.name();
-      const email = chance.email();
-      const user = await service.register({
-        firstName,
-        email,
-        password: chance.word(),
+      const chance = new Chance(42);
+      const userData = {
+        firstName: chance.name(),
+        email: chance.email(),
+        password: chance.word({ length: 10 }),
         imageUrl: '',
         additional: {},
-      });
+      };
 
+      jest.spyOn(User, 'query').mockImplementation(() => {
+        return {
+          findOne: jest.fn().mockReturnValue(null),
+          insert: jest.fn().mockReturnValue({ id: 1, ...userData }),
+        } as any;
+      });
+      jest.spyOn(UserCategory, 'query').mockImplementation(() => {
+        return {
+          where: jest.fn().mockReturnThis(),
+          count: jest.fn().mockReturnValue([{ count: 0 }]),
+        } as any;
+      });
+      const user = await service.register(userData);
       expect(user).toBeDefined();
-      expect(user.firstName).toBe(firstName);
-      expect(user.email).toBe(email);
+      expect(user.firstName).toBe(userData.firstName);
+      expect(user.email).toBe(userData.email);
+      expect(user).toMatchSnapshot();
     });
   });
 });

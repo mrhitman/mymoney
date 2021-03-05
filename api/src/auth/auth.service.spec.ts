d3a41afc55dc18cb7c5a756c32f049a247d30f74
One <kabalx47@gmail.com>
2021-03-05 18:54:36 +0200

    add validate user tests

--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -1,9 +1,11 @@
 import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import bcrypt from 'bcryptjs';
 import { Chance } from 'chance';
 import { config } from 'src/config';
 import { DatabaseModule } from 'src/database/database.module';
 import Category from 'src/database/models/category.model';
+import RefreshToken from 'src/database/models/refresh-token.model';
 import UserCategory from 'src/database/models/user-category.model';
 import User from 'src/database/models/user.model';
 import { UsersService } from 'src/users/users.service';
@@ -11,6 +13,14 @@ import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const chance = new Chance(42);
+  const userData = {
+    firstName: chance.name(),
+    email: chance.email(),
+    password: chance.word({ length: 10 }),
+    imageUrl: '',
+    additional: {},
+  };
 
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -32,15 +42,43 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
+  describe(' login', () => {
+    beforeEach(async () => {
+      const password = await bcrypt.hash(userData.password, 10);
+
+      (<jest.Mock>User.query).mockImplementation(() => ({
+        where: jest.fn().mockReturnThis(),
+        first: jest.fn().mockReturnValue({
+          id: 1,
+          ...userData,
+          password,
+        }),
+      }));
+      (<jest.Mock>RefreshToken.query).mockImplementation(() => ({
+        findOne: jest.fn().mockReturnValue({ id: 1 }),
+      }));
+    });
+
+    it(' validate user, exists', async () => {
+      const user = await service.validateUser(
+        userData.email,
+        userData.password,
+      );
+      expect(user).toBeDefined();
+      expect(user).not.toHaveProperty('password');
+      expect(user).toMatchSnapshot();
+    });
+
+    it(' validate user, not exists', async () => {
+      const user = await service.validateUser(
+        userData.email,
+        chance.word({ length: 10 }),
+      );
+      expect(user).not.toBeDefined();
+    });
+  });
+
   describe(' register User', () => {
-    const chance = new Chance(42);
-    const userData = {
-      firstName: chance.name(),
-      email: chance.email(),
-      password: chance.word({ length: 10 }),
-      imageUrl: '',
-      additional: {},
-    };
     beforeEach(() => {
       (<jest.Mock>User.query).mockImplementation(() => ({
         findOne: jest.fn().mockReturnValue(null),
